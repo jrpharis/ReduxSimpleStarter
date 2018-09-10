4cac9ed9a3536524600ebdea30345fe65ebc3949
Josh Pharis <jpharis@jdbyrider.com>
2018-09-10 01:27:35 -0400

    Creating simple YouTube search player

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/searchBar';
+import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCURo6vVzgYsI-mdZrX8G3u-104ygDNQPU';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        console.log('Initializing state');
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        console.log('Searching YouTube');
+        this.videoSearch('snowboarding');
+    }
+
+    videoSearch(term){
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            console.log('set videos: length = ' + videos.length);
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+             });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+        return ( 
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render( <App /> , document.querySelector('.container'));
