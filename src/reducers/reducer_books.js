aabc0e3ec53a0272e857d0d8fa2b18346bd275cc
Josh Pharis <jpharis@jdbyrider.com>
2018-09-11 02:19:00 -0400

    Book list application

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+function booksReducer() {
+  return [
+    { title: "Of Ice and Fire" , pages: 798},
+    { title: "Ready Player One", pages: 287},
+    { title: "React: Beginners", pages: 187},
+    { title: "Harry Potter", pages: 435}
+  ];
+}
+
+export default booksReducer;
